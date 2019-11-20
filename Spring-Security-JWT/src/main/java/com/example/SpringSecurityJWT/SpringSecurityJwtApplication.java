1da814f57612ea606e500d525d855509d46222c4
vijaybhaskarreddyn <vijareddy4277@gmail.com>
2019-11-20 15:07:18 +0530

    add one line

---
+++ Spring-Security-JWT/src/main/java/com/example/SpringSecurityJWT/SpringSecurityJwtApplication.java
@@ -0,0 +1,14 @@
+package com.example.SpringSecurityJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwtApplication.class, args);
+		System.out.println("Hello Spring security JWT");
+	}
+
+}
